f32d7300e118a48e76c7dceb0cf0159c5c6c2328
puyomi <revexderartist@gmail.com>
2025-11-11 17:03:21 +0100

    Erste Version auf GitHub
    Start des Packs auf GitHub zur besseren Zusammenarbeit des Projektteams

---
+++ assets/minecraft/lang/dupe.bat
@@ -0,0 +1,9 @@
+@echo off
+copy /y de_de.* de_at.*
+copy /y de_de.* de_ch.*
+copy /y de_de.* en_us.*
+copy /y de_de.* en_gb.*
+copy /y de_de.* en_au.*
+copy /y de_de.* en_ca.*
+copy /y de_de.* en_nz.*
+exit
